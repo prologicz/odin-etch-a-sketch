b1337fda2b13ae239029da4039db5202ab090f5f
Mecha Z <czbills88@gmail.com>
2022-09-07 12:28:01 -0400

    Color div element in grid on mouseover

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 console.log('Hello World');
 
 const gridContainer = document.querySelector('.grid-container');
+//gridContainer.style.gridTemplateColumns = 'repeat(10, 5em)';
+
 for (i=0; i < 256; i++){
     const newDiv = document.createElement('div');
-    newDiv.classList.add(`div-${i}`);
+    newDiv.classList.add('grid-box');
+    newDiv.id = `div-${i}`
     gridContainer.appendChild(newDiv);
 
 }
+
+const gridBoxes = document.querySelectorAll('.grid-box');
+gridBoxes.forEach(box => box.addEventListener('mouseover', (e) => {
+
+    e.target.classList.add('grid-box-mouseover');
+    console.log(e.target.id);
+    console.log(e.target.classList);
+}))
+
+console.log(gridBoxes);
